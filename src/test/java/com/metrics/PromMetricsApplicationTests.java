479ea209ea7656833bb10b8e24697ed0643fede8
priyadasgupta <57942438+priyadasgupta@users.noreply.github.com>
2021-06-08 12:32:34 +0530

    Create PromMetricsApplicationTests.java

---
+++ src/test/java/com/metrics/PromMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
