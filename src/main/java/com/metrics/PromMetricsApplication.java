6d71ad5808323e0e890e7b7122cf6ba1adbfe16f
priyadasgupta <57942438+priyadasgupta@users.noreply.github.com>
2021-06-08 12:31:14 +0530

    Create PromMetricsApplication.java

---
+++ src/main/java/com/metrics/PromMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromMetricsApplication.class, args);
+	}
+
+}
